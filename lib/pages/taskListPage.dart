d36b4fe76b3b82280f178b2ae949fd49804db44c
Rdemont <rdemont@rmbi.ch>
2024-04-21 15:11:08 +0200

    import error

--- lib/pages/taskListPage.dart
+++ lib/pages/taskListPage.dart
@@ -1,8 +1,4 @@
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tasker/businessObj/tagList.dart';
 import 'package:tasker/businessObj/taskGroupList.dart';
 import 'package:tasker/generate/businessObj/tagGen.dart';
